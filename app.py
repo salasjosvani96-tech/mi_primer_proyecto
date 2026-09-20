14acbcf6bd26918ca5f5893d4bcc45a1e9e84654
salasjosvani96-tech <salasjosvani96@gmail.com>
2026-09-19 22:47:05 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route("/acerca")
+def acerca():
+    return render_template("acerca.html")
+
+
+if __name__ == "__main__":app.run(debug=True)
